80d600ba3fb15c47c311b67099f67559f267663a
HaoTong <Haotong-github>
2016-12-05 23:07:57 +0800

    this is 2

---
+++ Log4jTtest/src/com/atguigu/dao/UserDaoImpl.java
@@ -0,0 +1,16 @@
+package com.atguigu.dao;
+
+import org.apache.log4j.Logger;
+
+public class UserDaoImpl {
+
+	private static final Logger logger = Logger.getLogger(UserDaoImpl.class);
+
+	public static void main(String[] args) {
+
+		logger.debug("----debug");
+		logger.warn("----warn");
+		logger.error("----error");
+		logger.info("finally");
+	}
+}
