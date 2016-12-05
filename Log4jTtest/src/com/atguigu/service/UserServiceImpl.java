45b3b4eda9fb9159fefed9b19a916f02f3ce27ed
HaoTong <Haotong-github>
2016-12-05 23:06:00 +0800

    this is 1

---
+++ Log4jTtest/src/com/atguigu/service/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.atguigu.service;
+
+import org.apache.log4j.Logger;
+
+public class UserServiceImpl {
+
+	private static final Logger logger = Logger.getLogger(UserServiceImpl.class);
+
+	public static void main(String[] args) {
+		logger.debug("");
+	}
+}
